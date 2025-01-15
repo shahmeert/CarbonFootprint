e186852fc6e9f73fb980757dc4f71b74c28f695a
shahmeert <shahmeert511@gmail.com>
2025-01-15 13:05:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculate(){
     const km = document.getElementById('km').value || 0;
     const electricity  = document.getElementById('electricity').value || 0;
 
-    const totalCO2 = (km * 30 * 0.12) + (electicity * 0.45);
+    const totalCO2 = (km * 30 * 0.12) + (electricity * 0.45);
 
     document.getElementById('result').innerText = 
         'Monthly CO2: ' + totalCO2.toFixed(2) + ' kg';
