de05482fea71c94a74f57221c3495ad3a165253a
Shahmeert <shahmeert511@gmail.com>
2025-01-15 12:49:03 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate(){
+    const km = document.getElementById('km').value || 0;
+    const electricity  = document.getElementById('electricity').value || 0;
+
+    const totalCO2 = (km * 30 * 0.12) + (electicity * 0.45);
+
+    document.getElementById('result').innerText = 
+        'Monthly CO2: ' + totalCO2.toFixed(2) + ' kg';
+}
